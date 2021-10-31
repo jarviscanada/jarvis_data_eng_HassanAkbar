58fda8d92768dbeeecf4ca83aced3d0c05119fa8
Hasan Akbar <akbars8989@gmail.com>
2021-10-31 01:54:19 +0000

    host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+ql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ $# -ne 5 ]; then
+    echo 'Please provide valid host, port, db name, user and password'
+    exit 1
+fi
+
+hostname=$(hostname -f)
+
+lscpu_out=`lscpu`
+
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs)
+timestamp=$(date + "%Y-%M-%d %H:%M:%S")
+
+
+insert_stmt="INSERT INTO host_info(hostname, cpu_number,cpu_architecture, cpu_model, cpu_mhz, l2_cache,total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model',  '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp')
+
+
+
