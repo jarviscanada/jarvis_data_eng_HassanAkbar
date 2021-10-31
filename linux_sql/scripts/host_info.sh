b47b4e1a9ee25680682080b9f4d77ba1e58920d9
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-10-31 00:16:50 -0400

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -26,8 +26,8 @@ timestamp=$(vmstat -t  | awk '{print $18,$19}'| tail -n1 | xargs)
 insert_stmt="INSERT INTO host_info(hostname, cpu_number,cpu_architecture, cpu_model, cpu_mhz, l2_cache,total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model',  '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp');"
 
 export PGPASSWORD=$psql_password 
-#Insert date into a database
-psql -h $psql_host -d $db_name -c "$insert_stmt"
+
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
 
 
