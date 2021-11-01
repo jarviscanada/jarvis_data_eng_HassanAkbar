78336a72711233d75525481f99188392c0018677
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-01 19:26:15 -0400

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -6,8 +6,8 @@ CREATE TABLE PUBLIC.host_info
      cpu_number       INTEGER NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
      cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          INTEGER NOT NULL,
-     L2_cache         INTEGER NOT NULL,
+     cpu_mhz          FLOAT NOT NULL,
+     L2_cache         VARCHAR NOT NULL,
      total_mem        INTEGER NOT NULL,
      "timestamp"      TIMESTAMP NOT NULL
   );
