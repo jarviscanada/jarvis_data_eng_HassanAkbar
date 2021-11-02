7b7824efe9717edd15e263b3296fb155a10bd68c
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-01 22:59:10 -0400

    Update host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -14,18 +14,18 @@ fi
 vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 
-timestamp=$(vmstat -t  | awk '{print $18,$19}'| tail -n1 | xargs)
+timestamp=$(date + "%Y-%m-%d %H:%M:%S")
 memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}'| tail -n1 | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $13}'| tail -n1 | xargs)
 disk_io=$(vmstat -d | awk '{print $10}'| tail -n1 | xargs)
-disk_available=$(df -BM / | awk '{print $4}')
+disk_available=$(df -BM / |egrep "^/dev/sda2" |awk '{print $4}') | sed 's/.$s//' | xargs)
 
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname');"
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
 
 insert_stmt="INSERT INTO host_usage(timestamp, host_id,memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle',  '$cpu_kernel', '$disk_io', '$disk_available');"
 
-export PGPASSWORD=$psql_password 
+export PGPASSWORD=$5
 
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
