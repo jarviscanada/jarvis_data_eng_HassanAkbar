775ff3613e0d900b2c4b5a2892a602f6dbf5c1f1
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-01 21:57:39 -0400

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -5,8 +5,8 @@ CREATE TABLE PUBLIC.host_info
      hostname         VARCHAR NOT NULL, 
      cpu_number       INTEGER NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
-     cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          FLOAT NOT NULL,
+     cpu_model        INTEGER NOT NULL,
+     cpu_mhz          INTEGER NOT NULL,
      L2_cache         VARCHAR NOT NULL,
      total_mem        INTEGER NOT NULL,
      "timestamp"      TIMESTAMP NOT NULL
@@ -19,14 +19,11 @@ CREATE TABLE PUBLIC.host_info
 CREATE TABLE PUBLIC.host_usage
   (
      "timestamp"      TIMESTAMP NOT NULL,
-     host_id	      SERIAL NOT NULL,
+     host_id	      SERIAL NOT NULL REFERENCES host_info(id),
      memory_free      INTEGER NOT NULL,
      cpu_idle         INTEGER NOT NULL,
      cpu_kernel       INTEGER NOT NULL,
      disk_io          INTEGER NOT NULL,
-     disk_available   BYTEA NOT NULL
-     CONSTRAINT fk_host 
-	FOREIGN KEY(host_id) 
-		REFERENCE PUBLIC.host_info(id)
+     disk_available   INTEGER NOT NULL
 );
 
