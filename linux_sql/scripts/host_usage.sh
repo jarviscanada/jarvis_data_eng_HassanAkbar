c818d3fb3a9df1336efd1e78018f556272c3d43b
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-01 23:27:28 -0400

    Update host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -19,7 +19,7 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}'| tail -n1 | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $13}'| tail -n1 | xargs)
 disk_io=$(vmstat -d | awk '{print $10}'| tail -n1 | xargs)
-disk_available=$(df -BM / |egrep "^/dev/sda2" |awk '{print $4}') | sed 's/.$//' | xargs)
+disk_available=$(df -BM / |egrep "^/dev/sda2" |awk '{print $4}') |sed 's/.$//' | xargs)
 
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
 
