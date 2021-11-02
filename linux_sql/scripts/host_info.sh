f13b27d3f56d67d0c508f655e061e61ebc3ac15c
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-01 22:43:29 -0400

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 psql_host=$1
 psql_port=$2
 db_name=$3
@@ -20,7 +21,7 @@ cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3}' | xargs
 cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
 l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs)
-timestamp=$(vmstat -t  | awk '{print $18,$19}'| tail -n1 | xargs)
+timestamp=$(date + "%Y-%m-%d %H:%M:%S")
 
 
 insert_stmt="INSERT INTO host_info(hostname, cpu_number,cpu_architecture, cpu_model, cpu_mhz, l2_cache,total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model',  '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp');"
