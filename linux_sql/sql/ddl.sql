3e49ede14bd4ae91e571fab043942af70d648d77
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-02 22:58:20 -0400

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -16,11 +16,12 @@ CREATE TABLE PUBLIC.host_info
 CREATE TABLE PUBLIC.host_usage
   (
      "timestamp"      TIMESTAMP NOT NULL,
-     host_id	      SERIAL NOT NULL REFERENCES host_info(id),
+     host_id	        SERIAL NOT NULL,
      memory_free      INTEGER NOT NULL,
      cpu_idle         INTEGER NOT NULL,
      cpu_kernel       INTEGER NOT NULL,
      disk_io          INTEGER NOT NULL,
-     disk_available   INTEGER NOT NULL
+     disk_available   INTEGER NOT NULL,
+     FOREIGN KEY (id) REFERENCES host_info (id)
 );
 
