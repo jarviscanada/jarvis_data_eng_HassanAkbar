d8ee5fa166505d9184b6af11d17c32f0b3627ad4
Hasan Akbar <92801629+Hasanakbar12@users.noreply.github.com>
2021-11-08 10:12:33 -0500

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#! /bin/bash
 
+#Assign arguments
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
+#validate arguments
 if [ $# -ne 5 ]; then
     echo 'Please provide valid host, port, db name, user and password'
     exit 1
 fi
 
+#parsing hardware specification
 hostname=$(hostname -f)
-
 lscpu_out=`lscpu`
 
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
@@ -24,13 +26,16 @@ l2_cache=${l2_cache1//[^[:digit:].-]/}
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}'| xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-
+#Insert host hardware info values into postgreSQL host_info table
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', $cpu_number, '$cpu_architecture', $cpu_model, $cpu_mhz, $l2_cache, $total_mem, '$timestamp');"
 
+#set up env var for psql cmd
 export PGPASSWORD=$psql_password 
 
+#Insert date into a database
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
-exit $?
+
+exit 0
 
 
 
